2fc29e4be438a8dd21a946f1c5b506af260f7d0f
Swasthi N V <138643318+Swasthii@users.noreply.github.com>
2024-03-30 19:21:23 +0530

    Update main.c

--- .github/workflows/main.c
+++ .github/workflows/main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
+int main() {
+    char string[400];
+    printf("Enter a String: ");
+    scanf("%s", string);
+    int length = strlen(string);
+    char rev[100] = "";
+    int i = length - 1;
+    while (i >= 0) {
+        rev[length - 1 - i] = string[i];
+        i--;
+    }
+    if (strcmp(string, rev) == 0)
+        printf("Given string is a palindrome.\n");
+    else
+        printf("Given string is not a palindrome.\n");
+    return 0;
+}
